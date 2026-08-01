ed453d2770aef66186b7545a41c09f92b23d6b1a
JunaidWBWR <junaid@wbwr.io>
2026-08-01 19:15:50 +0200

    bulk added components and fixed tsconfig issue

---
+++ src/components/Icon.tsx
@@ -0,0 +1,44 @@
+import { FiHeart, FiUsers, FiClock, FiHome, FiCheckCircle } from 'react-icons/fi';
+import {
+	FaMapMarkedAlt,
+	FaHeartbeat,
+	FaCalendarAlt,
+	FaHandHoldingHeart,
+	FaUserNurse,
+	FaPhoneAlt,
+	FaEnvelope,
+	FaMapMarkerAlt,
+	FaFacebook,
+	FaTwitter,
+	FaInstagram,
+} from 'react-icons/fa';
+import type { IconName } from '../constants';
+
+const icons: Record<IconName, React.ComponentType<{ className?: string }>> = {
+	FiHeart,
+	FiUsers,
+	FiClock,
+	FiHome,
+	FiCheckCircle,
+	FaMapMarkedAlt,
+	FaHeartbeat,
+	FaCalendarAlt,
+	FaHandHoldingHeart,
+	FaUserNurse,
+	FaPhoneAlt,
+	FaEnvelope,
+	FaMapMarkerAlt,
+	FaFacebook,
+	FaTwitter,
+	FaInstagram,
+};
+
+interface Props {
+	name: IconName;
+	className?: string;
+}
+
+export default function Icon({ name, className }: Props) {
+	const Component = icons[name];
+	return <Component className={className} />;
+}
